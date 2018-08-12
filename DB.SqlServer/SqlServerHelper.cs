f8817f495b19c6085459ba8c1a21f6853d913ee3
CDLiuDeHua <CDLiuDeHua@outlook.com>
2018-08-12 14:51:37 +0800

    最牛逼的yield+IEnumerable<T>封装

--- DB.SqlServer/SqlServerHelper.cs
+++ DB.SqlServer/SqlServerHelper.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DB.Interface;
+using Model;
 
 namespace DB.SqlServer
 {
@@ -57,8 +59,136 @@ namespace DB.SqlServer
             }
 
             return (T)t;
-
-            //return default(T);
         }
+
+        #region 最牛逼的yield+IEnumerable<T>封装
+
+        #region  根据传入的条件执行Sql语句，并返回一个IEnumerable<T>类型的集合
+        /// <summary>
+        /// 根据传入的条件执行Sql语句，并返回一个IEnumerable<T>类型的集合
+        /// （注意传入的 T 必须约束为 where T : class, new()）
+        /// </summary>
+        /// <typeparam name="T">类型：【 约束为 where T : class, new() 】</typeparam>
+        /// <param name="where">查询的条件，请省略 Where 关键字</param>
+        /// <returns></returns>
+        public IEnumerable<T> GetList<T>(string where)
+        {
+            Type type = typeof(T);
+            //遍历获得字段
+            string columnString = string.Join(",", type.GetProperties().Select(p => string.Format("[{0}]", p.Name)));
+            string sql = string.Format("SELECT {0} FROM [{1}] WHere {2} ",
+                columnString,
+                type.Name,
+                where);
+            using (SqlDataReader reader = ExecuteReader(sql))
+            {
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        yield return MapEntity<T>(reader);
+                    }
+                }
+            }
+        }
+        #endregion
+
+        #region ExecuteReader +static SqlDataReader ExecuteReader(string cmdText, params SqlParameter[] parameters)
+        /// <summary>
+        /// 执行一个查询的T-SQL语句, 返回一个SqlDataReader对象, 如果出现SQL语句执行错误, 将会关闭连接通道抛出异常
+        ///  ( 注意：调用该方法后，一定要对SqlDataReader进行Close )
+        /// </summary>
+        /// <param name="cmdText">要执行的T-SQL语句</param>
+        /// <param name="parameters">参数列表</param>
+        /// <exception cref="链接数据库异常"></exception>
+        /// <returns>SqlDataReader对象</returns>
+        public static SqlDataReader ExecuteReader(string cmdText, params SqlParameter[] parameters)
+        {
+            return ExecuteReader(cmdText, CommandType.Text, parameters);
+        }
+        #endregion
+
+        #region ExecuteReader +static SqlDataReader ExecuteReader(string cmdText, CommandType type, params SqlParameter[] parameters)
+        /// <summary>
+        /// 执行一个查询的T-SQL语句, 返回一个SqlDataReader对象, 如果出现SQL语句执行错误, 将会关闭连接通道抛出异常
+        ///  ( 注意：调用该方法后，一定要对SqlDataReader进行Close )
+        /// </summary>
+        /// <param name="cmdText">要执行的T-SQL语句</param>
+        /// <param name="type">命令类型</param>
+        /// <param name="parameters">参数列表</param>
+        /// <exception cref="链接数据库异常"></exception>
+        /// <returns>SqlDataReader对象</returns>
+        public static SqlDataReader ExecuteReader(string cmdText, CommandType type, params SqlParameter[] parameters)
+        {
+            SqlConnection conn = new SqlConnection(ConnectionStringCustomers);
+            using (SqlCommand cmd = new SqlCommand(cmdText, conn))
+            {
+                if (parameters != null)
+                {
+                    cmd.Parameters.Clear();
+                    cmd.Parameters.AddRange(parameters);
+                }
+                cmd.CommandType = type;
+                conn.Open();
+                try
+                {
+                    SqlDataReader reader = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+                    cmd.Parameters.Clear();
+                    return reader;
+                }
+                catch (System.Data.SqlClient.SqlException ex)
+                {
+                    //出现异常关闭连接并且释放
+                    conn.Close();
+                    throw ex;
+                }
+            }
+        }
+        #endregion
+
+        #region 将一个SqlDataReader对象转换成一个实体类对象 +static T MapEntity<T>(SqlDataReader reader) where T : class,new()
+        /// <summary>
+        /// 将一个SqlDataReader对象转换成一个实体类对象
+        /// </summary>
+        /// <typeparam name="T">实体类型</typeparam>
+        /// <param name="reader">当前指向的reader</param>
+        /// <returns>实体对象</returns>
+        public static T MapEntity<T>(SqlDataReader reader)
+        {
+            try
+            {
+                Type type = typeof(T);
+                var props = type.GetProperties();
+
+                object entity = Activator.CreateInstance(type);//创建返回的单个对象
+                foreach (var prop in props)
+                {
+                    if (prop.CanWrite)
+                    {
+                        try
+                        {
+                            var index = reader.GetOrdinal(prop.Name);
+                            var data = reader.GetValue(index);
+                            if (data != DBNull.Value)
+                            {
+                                prop.SetValue(entity, Convert.ChangeType(data, prop.PropertyType), null);
+                            }
+                        }
+                        catch (IndexOutOfRangeException)
+                        {
+                            continue;
+                        }
+                    }
+                }
+                return (T)entity;
+            }
+            catch
+            {
+                return default(T);
+            }
+        }
+        #endregion
+
+        #endregion
     }
 }
--- MyReflection/Program.cs
+++ MyReflection/Program.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using DB.Interface;
 using DB.SqlServer;
+using Model;
 
 namespace MyReflection
 {
@@ -76,19 +77,138 @@ namespace MyReflection
                     object oTest = Activator.CreateInstance(type, new object[] { 123 });
                     object oTest1 = Activator.CreateInstance(type, new object[] { "ssss" });
 
-
                     Type Singletontype = assembly.GetType("DB.SqlServer.Singleton");
                     Singleton singleton = Singleton.GetInstance();
-                    //通过反射破坏单例
+
+                    //通过反射破坏单例，调用私有方法
                     object oSingletion1 = Activator.CreateInstance(Singletontype, true);
                     object oSingletion2 = Activator.CreateInstance(Singletontype, true);
                     object oSingletion3 = Activator.CreateInstance(Singletontype, true);
 
-                    //泛型调用
+                    //泛型调用类
                     Type Generictype = assembly.GetType("DB.SqlServer.GenericClass`3");
                     Type markType = Generictype.MakeGenericType(typeof(int), typeof(int), typeof(string));//创建对象必须使用此对象，因为这个已经指定了类型
                     object oGeneric = Activator.CreateInstance(markType);
 
+
+                    //GenericMethod
+
+                }
+
+                #endregion
+
+                #region 反射调用实例方法、静态方法、重载方法 
+                {
+                    Console.WriteLine("************************  反射调用实例方法、静态方法、重载方法  ******************************");
+                    Assembly assembly = Assembly.Load("DB.SqlServer");
+                    Type type = assembly.GetType("DB.SqlServer.ReflectionTest");
+                    object oTest = Activator.CreateInstance(type);
+
+                    foreach (var item in type.GetMethods())
+                    {
+                        Console.WriteLine(item.Name);
+                    }
+
+
+                    {
+                        Console.WriteLine("************************  调用方法实例  ******************************");
+                        MethodInfo method = type.GetMethod("Show1");
+                        method.Invoke(oTest, null);
+                    }
+                    {
+                        Console.WriteLine("************************  调用静态方法实例  ******************************");
+                        MethodInfo method = type.GetMethod("Show5");
+                        method.Invoke(null, new object[] { "装逼" });
+                    }
+                    {
+                        Console.WriteLine("************************  调用重载方法实例  ******************************");
+                        MethodInfo method = type.GetMethod("Show3", new Type[] { typeof(int), typeof(string) });
+                        method.Invoke(oTest, new object[] { 123, "装逼" });
+
+                        MethodInfo method1 = type.GetMethod("Show3", new Type[] { typeof(string), typeof(int) });
+                        method1.Invoke(oTest, new object[] { "装逼", 123 });
+                    }
+                }
+                #endregion
+
+
+                #region 反射调用    私有+泛型方法
+
+                {
+                    Assembly assembly = Assembly.Load("DB.SqlServer");
+
+                    {
+                        Console.WriteLine("************************  调用私有方法  ******************************");
+                        Type type = assembly.GetType("DB.SqlServer.ReflectionTest");//获取类型
+
+                        object oTest = Activator.CreateInstance(type);
+                        MethodInfo method = type.GetMethod("Show4", BindingFlags.Instance | BindingFlags.NonPublic);
+                        method.Invoke(oTest, new object[] { "小刘" });
+                    }
+
+                    {
+                        Console.WriteLine("************************  调用泛型方法  ******************************");
+                        Type type = assembly.GetType("DB.SqlServer.GenericMethod");//获取类型
+                        object oTest = Activator.CreateInstance(type);
+
+                        MethodInfo method = type.GetMethod("Show");
+                        MethodInfo methodNew = method.MakeGenericMethod(typeof(int), typeof(int), typeof(string));
+                        methodNew.Invoke(oTest, new object[] { 123, 123, "泛型" });
+
+                    }
+
+                }
+
+                #endregion
+
+                #region 字段属性
+
+                {
+                    Console.WriteLine("************************  字段属性  ******************************");
+                    //People people = new People();
+                    Type type = typeof(People);
+                    object oPeople = Activator.CreateInstance(type);
+
+                    //foreach (var item in type.GetProperties())
+                    foreach (var item in type.GetFields())
+                    {
+                        //获取名称，值
+
+                        if (item.Name.Equals("Id"))
+                        {
+                            item.SetValue(oPeople, 123);
+                        }
+                        else if (item.Name.Equals("Name"))
+                        {
+                            item.SetValue(oPeople, "小猪佩奇");
+                        }
+                        else if (item.Name.Equals("Description"))
+                        {
+                            item.SetValue(oPeople, "不是一只猪");
+                        }
+                    }
+
+
+                }
+
+                #endregion
+
+
+
+                #region SQL
+
+                {
+
+                    SqlServerHelper sql = new SqlServerHelper();
+                    var list = sql.GetList<Model.Book>(" ID>100 ");
+
+                    foreach (var item in list)
+                    {
+                        Console.WriteLine(item.Id + "***" + item.ISBN + "***" + item.PublishDate + "***" + item.PublisherId + "***" + item.Title + "***" + item.TOC + "***" + item.UnitPrice + "***" + item.WordsCount);
+                       
+                    }
+
+                    Console.WriteLine(list.Count());
                 }
 
                 #endregion
